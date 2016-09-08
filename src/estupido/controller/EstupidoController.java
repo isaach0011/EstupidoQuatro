eaa0971f58393e6ac797bc3fe731ccf133015e48
isaach0011 <ihil17523@740s-pl-177.local>
2016-09-08 13:18:46 -0600

    Added user input.

--- src/estupido/controller/EstupidoController.java
+++ src/estupido/controller/EstupidoController.java
@@ -1,17 +1,29 @@
 package estupido.controller;
 
+import java.util.Scanner;
+
 public class EstupidoController
 {
 	private String myName;
+	private Scanner readKeyboardInput;
 	
 	public EstupidoController()
 	{
 		myName = "Isaac Hill";
+		readKeyboardInput = new Scanner(System.in);
 	}
 	
 	public void start()
 	{
 		System.out.println("This is slightly less stupid :D");
 		System.out.println(myName);
+		askQuestions();
+	}
+	
+	private void askQuestions()
+	{
+		System.out.println("What is your name?");
+		String userInput = readKeyboardInput.nextLine();
+		System.out.println("You said " + userInput + ".");
 	}
 }
